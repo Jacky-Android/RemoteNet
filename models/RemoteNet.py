b163824eebdaf8476cbcdad9a2445b184b0c9a59
Silas <55181594+Jacky-Android@users.noreply.github.com>
2023-11-26 15:14:43 +0800

    Update RemoteNet.py

--- models/RemoteNet.py
+++ models/RemoteNet.py
@@ -8,7 +8,7 @@ from .Encoder import Encoder
 class RemoteNet(nn.Module):
     def __init__(self, dim = 64,dims = (32,64,160,256),num_classes=6):
         super().__init__()
-        self.Encoder = Encoder(dims=dims)
+        self.Encoder = Encoder()
         self.Decoder = Decoder(dim=dim,dims=dims,num_classes=num_classes)
     def forward(self, x):
         stage1,stage2,stage3,stage4  = self.Encoder(x)
